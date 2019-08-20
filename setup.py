3727d95ad8f42e5d14a03bc032ac8e29ba6d610e
Katja Suess <k.suess@rohberg.ch>
2019-08-20 13:48:29 +0200

    Skip dependency dexterity.membrane

--- setup.py
+++ setup.py
@@ -45,8 +45,6 @@ setup(
         'Products.GenericSetup>=1.8.2',
         'setuptools',
         'z3c.jbot',
-        'Products.membrane',
-        'dexterity.membrane',
         'collective.address',
         'collective.dexteritytextindexer',
     ],
--- src/rohberg/contentmember/content/zhkathauthor.py
+++ src/rohberg/contentmember/content/zhkathauthor.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 from collective import dexteritytextindexer
-from dexterity.membrane.behavior.user import INameFromFullName
-from dexterity.membrane.content.member import IMember
-from plone.autoform.directives import widget
+from plone.app.textfield import RichText
+from plone.app.z3cform.widget import RichTextFieldWidget
+from plone.autoform import directives
 from plone.dexterity.content import Item
-# from plone.supermodel import model
+from plone.supermodel import model
+from plone.schema import Email
 from rohberg.contentmember.fields import URI_noprotocol
 from zope import schema
 from zope.interface import implementer
@@ -12,10 +13,42 @@ from zope.interface import implementer
 from rohberg.contentmember import _
 
 
+class IMember(model.Schema):
+    """Define Author."""
+
+    first_name = schema.TextLine(
+        title=_(u'First Name'),
+        required=True,
+    )
+
+    last_name = schema.TextLine(
+        title=_(u'Last Name'),
+        required=True,
+    )
+
+    email = Email(
+        # String with validation in place looking for @, required.
+        # Note that a person's email address will be their username.
+        title=_(u'E-mail Address'),
+        required=True,
+    )
+    #
+    # homepage = schema.URI(
+    #     # url format
+    #     title=_(u'External Homepage'),
+    #     required=False,
+    # )
+
+    directives.widget('bio', RichTextFieldWidget)
+    bio = RichText(
+        title=_(u'Biography'),
+        required=False,
+    )
+
+
 class IZhkathauthor(IMember):
     """Marker interface for Zhkathauthor."""
 
-    # TODO: make membrane fields searchable
     dexteritytextindexer.searchable('first_name')
     dexteritytextindexer.searchable('last_name')
     dexteritytextindexer.searchable('companyposition')
@@ -50,7 +83,7 @@ class IZhkathauthor(IMember):
         required=False
     )
 
-    widget(authortype='z3c.form.browser.checkbox.CheckBoxFieldWidget')
+    directives.widget(authortype='z3c.form.browser.checkbox.CheckBoxFieldWidget')
     authortype = schema.Set(
         title=_(u"label_authortype", default=u"Typ"),
         value_type=schema.Choice(values=['Autor', 'Kontakt']),
@@ -63,16 +96,17 @@ class IZhkathauthor(IMember):
     )
 
 
-IZhkathauthor['homepage'].title = u"TODO: hide this field homepage"
-
-
 @implementer(IZhkathauthor)
 class Zhkathauthor(Item):
     """
     """
     def Title(self):
-        return INameFromFullName(self).title
+        return self.title
 
     @property
     def title(self):
-        return INameFromFullName(self).title
+        names = [
+            self.first_name,
+            self.last_name,
+        ]
+        return u' '.join([name for name in names if name])
