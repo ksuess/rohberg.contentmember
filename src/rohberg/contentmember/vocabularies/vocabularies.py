ad84c97b732a58a21eebc518b02092b44c72d6b2
ksuess <k.suess@rohberg.ch>
2021-06-25 18:28:11 +0200

    Update vocabularies.py

--- src/rohberg/contentmember/vocabularies/vocabularies.py
+++ src/rohberg/contentmember/vocabularies/vocabularies.py
@@ -3,8 +3,10 @@ from plone.app.vocabularies.catalog import KeywordsVocabulary
 from zope.interface import implementer
 from zope.schema.interfaces import IVocabularyFactory
 
+
 @implementer(IVocabularyFactory)
 class AuthortypeVocabulary(KeywordsVocabulary):
     keyword_index = 'authortype'
+
 
 AuthortypeVocabularyFactory = AuthortypeVocabulary()
