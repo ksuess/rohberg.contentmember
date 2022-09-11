e6fb4578c5f9c975e8fc3239bcd677c1f214095c
Katja Süss <k.suess@rohberg.ch>
2022-09-11 15:06:51 +0200

    1.0a2

--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-long_description = '\n\n'.join([
-    open('README.rst').read(),
-    open('CONTRIBUTORS.rst').read(),
-    open('CHANGES.rst').read(),
-])
+long_description = "\n\n".join(
+    [
+        open("README.rst").read(),
+        open("CONTRIBUTORS.rst").read(),
+        open("CHANGES.rst").read(),
+    ]
+)
 
 
 setup(
-    name='rohberg.contentmember',
-    version='1.0a1',
+    name="rohberg.contentmember",
+    version="1.0a2",
     description="member is a content item to search for, with views, etc",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -28,37 +30,37 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    keywords='Python Plone',
-    author='Katja Suess',
-    author_email='k.suess@rohberg.ch',
-    url='https://pypi.python.org/pypi/rohberg.contentmember',
-    license='GPL version 2',
-    packages=find_packages('src', exclude=['ez_setup']),
-    namespace_packages=['rohberg'],
-    package_dir={'': 'src'},
+    keywords="Python Plone",
+    author="Katja Suess",
+    author_email="k.suess@rohberg.ch",
+    url="https://pypi.python.org/pypi/rohberg.contentmember",
+    license="GPL version 2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    namespace_packages=["rohberg"],
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         # -*- Extra requirements: -*-
-        'plone.app.dexterity',
-        'plone.api>=1.8.4',
-        'Products.GenericSetup>=1.8.2',
-        'setuptools',
-        'z3c.jbot',
-        'dexterity.membrane>=3.0.0a1',
-        'Products.membrane>=5.0.0a1',
-        'collective.address',
-        'collective.dexteritytextindexer',
+        "plone.app.dexterity",
+        "plone.api>=1.8.4",
+        "Products.GenericSetup>=1.8.2",
+        "setuptools",
+        "z3c.jbot",
+        "dexterity.membrane>=3.0.0a1",
+        "Products.membrane>=5.0.0a1",
+        "collective.address",
+        "collective.dexteritytextindexer",
     ],
     extras_require={
-        'test': [
-            'plone.app.testing',
+        "test": [
+            "plone.app.testing",
             # Plone KGS does not use this version, because it would break
             # Remove if your package shall be part of coredev.
             # plone_coredev tests as of 2016-04-01.
-            'plone.testing>=5.0.0',
-            'plone.app.contenttypes',
-            'plone.app.robotframework[debug]',
+            "plone.testing>=5.0.0",
+            "plone.app.contenttypes",
+            "plone.app.robotframework[debug]",
         ],
     },
     entry_points="""
