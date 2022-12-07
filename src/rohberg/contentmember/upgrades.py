39b2ef09d1bde9d2956ebbbab43646ec4bc48f4c
Katja Süss <k.suess@rohberg.ch>
2022-12-07 09:35:54 +0100

    Add last_name to catalog metadata and make content sortable by last_name

--- src/rohberg/contentmember/upgrades.py
+++ src/rohberg/contentmember/upgrades.py
@@ -1,41 +1,54 @@
 # -*- coding: utf-8 -*-
 from plone import api
-from plone.app.upgrade.utils import loadMigrationProfile
+
+# from plone.app.upgrade.utils import loadMigrationProfile
 import logging
+
+
+PROFILE_ID = "profile-rohberg.contentmember:default"
 
 
 def indexAuthors(context, logger=None):
     if logger is None:
         # Called as upgrade step: define our own logger.
-        logger = logging.getLogger('your.addonpackage')
-    
-    PROFILE_ID = 'profile-rohberg.contentmember:default'
-    setup = api.portal.get_tool('portal_setup')
-    setup.runImportStepFromProfile(PROFILE_ID, 'catalog')
+        logger = logging.getLogger("your.addonpackage")
 
-    catalog = api.portal.get_tool('portal_catalog')
-    brains = catalog(portal_type='zhkathpage')  # author?
+    PROFILE_ID = "profile-rohberg.contentmember:default"
+
+    setup = api.portal.get_tool("portal_setup")
+    setup.runImportStepFromProfile(PROFILE_ID, "catalog")
+
+    catalog = api.portal.get_tool("portal_catalog")
+    brains = catalog(portal_type="zhkathpage")  # author?
     count = 0
     for brain in brains:
         obj = brain.getObject()
         obj.reindexObject()
 
-    setup.runImportStepFromProfile(PROFILE_ID, 'catalog')
-    logger.info('%s pages indexed.' % count)
+    setup.runImportStepFromProfile(PROFILE_ID, "catalog")
+    logger.info("%s pages indexed." % count)
 
 
-def indexEmail(context, logger=None):
+def updateCatalog(context, logger=None):
     if logger is None:
         # Called as upgrade step: define our own logger.
-        logger = logging.getLogger('rohberg.contentmember')
-    
-    PROFILE_ID = 'profile-rohberg.contentmember:default'
-    setup = api.portal.get_tool('portal_setup')
-    setup.runImportStepFromProfile(PROFILE_ID, 'catalog')
-    brains = api.content.find(portal_type='zhkathauthor')
-    logger.info(f"{len(brains)} objects reindexed")
+        logger = logging.getLogger("rohberg.contentmember")
+
+    setup = api.portal.get_tool("portal_setup")
+    setup.runImportStepFromProfile(PROFILE_ID, "catalog")
+    brains = api.content.find(portal_type="zhkathauthor")
     for brain in brains:
         obj = brain.getObject()
-        obj.reindexObject(idxs=[
-          'email',
-          ])
+        obj.reindexObject()
+    logger.info(f"{len(brains)} rohberg.contentmember objects reindexed")
+
+
+def index_last_name(context, logger=None):
+    updateCatalog(context)
+
+    if logger is None:
+        # Called as upgrade step: define our own logger.
+        logger = logging.getLogger("rohberg.contentmember")
+
+    setup = api.portal.get_tool("portal_setup")
+    setup.runImportStepFromProfile(PROFILE_ID, "plone.app.registry")
